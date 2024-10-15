3156d845357d005a5c2375470e75e4054a67a4d3
Lika <lika@gmail.com>
2024-10-15 20:53:15 +0300

    Linux Python
    - Домашнее задание-1

---
+++ Linux_Python/Homework_1/test_task1.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def run(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.returncode == 0:
+        return True
+    else:
+        return False
+
+def test_step1():
+    assert run("", "")
